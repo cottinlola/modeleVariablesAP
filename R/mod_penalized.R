bee56ca998be57709bfc333b50bfba7462250ad2
vadmbertr <36510417+vadmbertr@users.noreply.github.com>
2022-03-28 15:02:07 +0200

    coding style

--- R/mod_penalized.R
+++ R/mod_penalized.R
@@ -6,7 +6,7 @@
 #' @param y_var_name A character naming the variable to explain
 #' @param alpha A numeric to balance betwwen ridge (alpha=0) and lasso
 #'              (alpha=1, default) penalization
-#' @param type.measure  A character naming the error metric used for the
+#' @param type_measure  A character naming the error metric used for the
 #'                      cross-validation (defaults to "rmse")
 #'
 #' @return The fitted model
@@ -15,14 +15,14 @@
 #' mod <- mod_penalized(data, y_var_name = "SUBEX")
 #'
 mod_penalized <- function(data, X_vars_names = character(), y_var_name,
-                          alpha = 1, type.measure = "rmse") {
+                          alpha = 1, type_measure = "rmse") {
   if (length(X_vars_names) == 0) {
     data <- data[, X_vars_names]
   }
   X <- data[, -y_var_name]
   y <- data[, y_var_name]
   mod_en <- glmnet::cv.glmnet(x = as.matrix(X), y = y, alpha = alpha,
-                              type.measure = type.measure)
+                              type.measure = type_measure)
   mod_en$glmnet.fit$cvm <- mod_en$cvm
   mod_en$glmnet.fit$nzero <- mod_en$nzero
   mod_en$glmnet.fit$lambda.min <- mod_en$lambda.min
