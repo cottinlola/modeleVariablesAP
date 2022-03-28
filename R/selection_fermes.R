dd69be14536abb8b98740432ed58751d12a157f0
GAFFET Marie <gaffet.marie@gmail.com>
2022-03-28 16:08:45 +0200

    Sélection des fermes (qui sont présentes au moins 5 fois)

---
+++ R/selection_fermes.R
@@ -0,0 +1,13 @@
+#' Sélection des fermes présentes au moins X ans (par défaut 5 ans)
+#'
+#' @param data_ind le jeu de données (avec la variable IDNUM)
+#' @param nb_annees le nom de la variable à expliquer
+#' @return dataframe : le jeu de données avec les fermes présentes
+#' @examples
+#' selection_fermes(data_ind)
+
+selection_fermes <- function(data_ind, nb_annees = 5){
+  idnum <- table(data_ind$IDNUM)
+  data_ind_y <- data_ind[data_ind$IDNUM %in% names(idnum[idnum >= nb_annees]), ]
+  return(data_ind_y)
+}
