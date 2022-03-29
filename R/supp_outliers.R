dba3a9a30a50c546240a4c2d5fe92cc68227dbd2
vadmbertr <36510417+vadmbertr@users.noreply.github.com>
2022-03-29 15:24:37 +0200

    add custom cutoff to outliers detection

--- R/supp_outliers.R
+++ R/supp_outliers.R
@@ -1,15 +1,16 @@
 #' Suppression des outliers
 #'
 #' @param data le jeu de données
+#' @param custom_cutoff permet de spécifier un cutoff arbitraire
 #' @param graph pour représenter les points (TRUE ou FALSE)
 #' @return dataframe ou plot : le jeu de données sans les outliers ou la représentation graphique
 #'
 #' @export
 #'
 #' @examples
-#' supp_outliers(data_ind_5y, TRUE)
+#' supp_outliers(data_ind_5y, graph = TRUE)
 
-supp_outliers <- function(data, graph = FALSE){
+supp_outliers <- function(data, custom_cutoff = NULL, graph = FALSE){
   var_num <- setdiff(names(which(sapply(data, is.numeric))), c("IDNUM", "MILEX"))
   data_num <- data[, var_num]
   # 0n garde uniquement les variables avec suffisamment de variabilité
@@ -19,8 +20,11 @@ supp_outliers <- function(data, graph = FALSE){
   })
   data_num <- data_num[, names(data_num) %in% names(which(t < 50))]
   # On se base sur la distance de Mahalanobis
-  outliers <- mvoutlier::sign1(as.matrix(data_num), qcrit = .999)
-  cut_off <- outliers$const
+  outliers <- mvoutlier::sign1(as.matrix(data_num))
+  cut_off <- custom_cutoff
+  if (is.null(cut_off)) {
+    cut_off <- quantile(outliers$x.dist, .75) + 3 * iq
+  }
   data_no_outliers <- data[which(outliers$x.dist <= cut_off), ]
   # plot outliers
   if (graph == TRUE) {
