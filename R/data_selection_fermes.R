1cede41c10e7b3b19cb78be459beed5c85262d24
vadmbertr <36510417+vadmbertr@users.noreply.github.com>
2022-03-30 10:34:39 +0200

    add global function to prepare data

--- R/data_selection_fermes.R
+++ R/data_selection_fermes.R
@@ -1,6 +1,6 @@
 #' Sélection des fermes présentes au moins X ans (par défaut 5 ans)
 #'
-#' @param data_ind le jeu de données (avec la variable IDNUM)
+#' @param data le jeu de données (avec la variable IDNUM)
 #' @param nb_annees le nom de la variable à expliquer
 #' @return dataframe : le jeu de données avec les fermes présentes
 #'
@@ -9,8 +9,8 @@
 #' @examples
 #' selection_fermes(data_ind)
 
-selection_fermes <- function(data_ind, nb_annees = 5) {
-  idnum <- table(data_ind$IDNUM)
-  data_ind_y <- data_ind[data_ind$IDNUM %in% names(idnum[idnum >= nb_annees]), ]
+selection_fermes <- function(data, nb_annees = 5) {
+  idnum <- table(data$IDNUM)
+  data_ind_y <- data[data$IDNUM %in% names(idnum[idnum >= nb_annees]), ]
   return(data_ind_y)
 }
