e575b8d21b519b69d6c521bcf46f407c68829bbd
Cottin Lola <lola.cottin@aol.fr>
2022-03-30 10:51:53 +0200

    change barplot to ggplot

--- R/ind_vif.R
+++ R/ind_vif.R
@@ -1,21 +1,26 @@
 #' Calcul du VIF (pour étudier la multi-colinéarité) et graphique
 #' Si VIF proche de 1 : modèle + robuste
-#' car les facteurs ne sont pas influencés par la corrélation avec d'autres facteurs
+#' car les facteurs ne sont pas influencés par
+#' la corrélation avec d'autres facteurs
 #'
 #' @param mod le modèle
 #' @param graph un bouléen TRUE pour tracer le graphique du VIF ou FALSE sinon
-#' @return numeric ou plot : retourne les valeurs du VIF pour chaque variable explicative ou le graphique
+#' @return numeric ou plot : retourne les valeurs du VIF
+#'          pour chaque variable explicative ou le graphique
 #'
 #' @export
 #'
 #' @examples
 #' vif(modele_lineaire, graph = TRUE)
 
-ind_vif <- function(mod, graph = FALSE){
-  car::vif_values <- vif(mod)
-  if (graph == TRUE){
-    barplot(vif_values, main = "VIF Values", horiz = TRUE, col = "steelblue")
-    return(abline(v = 5, lwd = 3, lty = 2))
+ind_vif <- function(mod, graph = FALSE) {
+  vif_values <- car::vif(mod)
+  if (graph == TRUE) {
+    df <- data.frame(var_names = names(vif_values), value = vif_values)
+    p <- ggplot(df, aes(y = var_names, x = value)) +
+    geom_bar(stat = "identity", fill = "steelblue") +
+    theme_minimal() + labs(y = "", x = "VIF values")
+    return(p)
   }
   return(vif_values)
 }
